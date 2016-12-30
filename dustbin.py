ffade9b4c779df2f40cd56542fbbd58992299b26
Trixi <bozso.beatrix@gmail.com>
2016-12-31 00:43:53 +0100

    first version of dustbin.py

--- dustbin.py
+++ dustbin.py
@@ -5,4 +5,33 @@ from dustbin_content_error import DustbinContentError
 
 
 class Dustbin:
-    pass
+    def __init__(self, color, paper_content = None, plastic_content = None, house_waste_content = None):
+        self.color = color
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+
+    def throw_out_garbage(self, garbage):
+        if isinstance(garbage, PlasticGarbage):
+            if garbage.is_clean:
+                self.plastic_content.append(garbage)
+            else:
+                DustbinContentError()
+        elif isinstance(garbage, PaperGarbage):
+            if garbage.is_squeezed:
+                self.paper_content.append(garbage)
+            else:
+                DustbinContentError()
+        elif isinstance(garbage, Garbage):
+            self.house_waste_content.append(garbage)
+        else:
+            DustbinContentError()
+    
+    def empty_contents(self, paper_content = None, plastic_content = None, house_waste_content = None):
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+
+
+
+    
