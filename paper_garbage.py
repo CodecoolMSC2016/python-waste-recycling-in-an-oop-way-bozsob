75ee97dfe2461b086936d2856d87a65e7a0b2b7e
Trixi <bozso.beatrix@gmail.com>
2016-12-31 17:23:30 +0100

    def __init__() modified

--- paper_garbage.py
+++ paper_garbage.py
@@ -3,7 +3,7 @@ from garbage import Garbage
 
 class PaperGarbage(Garbage):
     def __init__(self, name, is_squeezed):
-        self.name = name
+        super().__init__(name)
         self.is_squeezed = False
     
     def squeeze(self):
--- plastic_garbage.py
+++ plastic_garbage.py
@@ -3,7 +3,7 @@ from garbage import Garbage
 
 class PlasticGarbage(Garbage):
     def __init__(self, name, is_clean):
-        self.name = name
+        super().__init__(name)
         self.is_clean = False
 
     def clean(self):
