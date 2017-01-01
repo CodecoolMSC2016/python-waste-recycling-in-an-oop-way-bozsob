d2a1b4da2db5dca48db13fe7b5ff8482a4d42e31
Trixi <bozso.beatrix@gmail.com>
2017-01-01 16:56:27 +0100

    throw_out_garbage func modified, Exceptions is ok now

--- dustbin.py
+++ dustbin.py
@@ -11,23 +11,27 @@ class Dustbin:
         self.plastic_content = []
         self.house_waste_content = []
 
+
     def throw_out_garbage(self, garbage):
+    
         if isinstance(garbage, PlasticGarbage):
             if garbage.is_clean:
                 self.plastic_content.append(garbage)
             else:
-                DustbinContentError
+                raise DustbinContentError
 
         elif isinstance(garbage, PaperGarbage):
             if garbage.is_squeezed:
                 self.paper_content.append(garbage)
             else:
-                DustbinContentError
+                raise DustbinContentError
 
         elif isinstance(garbage, Garbage):
             self.house_waste_content.append(garbage)
+               
         else:
-            DustbinContentError
+            raise DustbinContentError
+  
     
     def empty_contents(self, paper_content = None, plastic_content = None, house_waste_content = None):
         self.paper_content = []
